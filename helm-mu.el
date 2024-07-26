4a45bb87215a47c8638034a06bf6529369779c5c
Titus von der Malsburg <malsburg@posteo.de>
2024-07-26 08:06:12 +0200

    Use `compose-mail' instead of `mu4e~compose...'

--- helm-mu.el
+++ helm-mu.el
@@ -454,8 +454,8 @@ search in mu4e-headers view."
 
 (defun helm-mu-compose-mail (_candidate)
   "Compose a new email directed to the selected contacts."
-  (mu4e-compose-mail (mapconcat 'helm-mu-format-contact
-                                (helm-marked-candidates) ", ")))
+  (compose-mail (mapconcat 'helm-mu-format-contact
+                           (helm-marked-candidates) ", ")))
 
 (defun helm-mu-action-get-contact-emails (_candidate)
   "Get the emails from/to (marked) contact"
